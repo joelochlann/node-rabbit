a8e2a8d449b79bf582335caf9c00ebd42e75ff7f
Joseph Smith <joseph.smith@worldfirst.com>
2015-04-01 16:26:54 +0000

    Initial commit

---
+++ send.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$connection = new AMQPConnection('crmdevel2.worldfirst.local', 5672, 'crmuser', 'crmuserpassword');
+$channel = $connection->channel();
+
+$channel->queue_declare('hello', false, false, false, false);
+
+$msg = new AMQPMessage('Hello World!');
+$channel->basic_publish($msg, '', 'hello');
+
+echo " [x] Sent 'Hello World!'\n";
+
+$channel->close();
+$connection->close();
